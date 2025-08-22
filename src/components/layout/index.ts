128d4f86b3454e117de77eab549e48c079969a29
nvcho <ipmain07@gmail.com>
2025-08-22 12:33:12 -0600

    feat: temporally remove stupid zod

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,2 @@
-export { default as NavigationFooter } from "./NavigationFooter";
-export { default as Header } from "./Header";
-export { default as AuthRoot } from "./AuthRoot";
+export { default as NavigationFooter } from './NavigationFooter';
+export { default as Header } from './Header';
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import postgres from 'postgres';
-import { config } from '@/config/env';
 import { drizzle } from 'drizzle-orm/postgres-js';
 
-const client = postgres(config.databaseUrl, { prepare: false });
+const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 
 export const db = drizzle(client);
